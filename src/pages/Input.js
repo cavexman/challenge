31d833ef276148c81368a04fc852bbe61eeea031
caveman <caveman@mac.com>
2019-12-05 19:10:02 -0800

    cleanup console msgs

--- src/pages/Input.js
+++ src/pages/Input.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 // I considered validating the price field to force a numerical value, but what if a user wanted to type "none", or "Free"?
 // spec is unclear on this ;-)
-function Input(props) {
+export default function Input(props) {
   return (
       <div className={"field " + props.field_type}>
       <label className="field__label" htmlFor={props.name}>{props.label}</label>
